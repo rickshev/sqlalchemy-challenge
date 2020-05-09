1560c1328341084229047f112e8d46d567b09b79
rickshev <rick.shevlin@gmail.com>
2020-05-09 17:39:07 -0400

    Final Commit

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 import datetime as dt
 from flask import Flask, jsonify
-from flask_sqlalchemy import sqlalchemy
 
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False})
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -31,48 +31,76 @@ def home():
         f"/api/v1.0/precipitation</br>"
         f"/api/v1.0/stations</br>"
         f"/api/v1.0/tobs</br>"
-        f"/api/v1.0/<start></br>"
-        f"/api/v1.0/<start>/<end></br>"
+        f"/api/v1.0/<start_date></br>"
+        f"/api/v1.0/<start_date>/<end_date></br>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    results_prec = session.query(measurement.date, measurement.prcp).filter(measurement.date >= "2016-08-23").all()
 
+    precip_list = list(np.ravel(results_prec))
+    
+    return jsonify(precip_list)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    results = session.query(station.name).all()
-    station_list = list(np.ravel(results))
+    results_stat = session.query(station.name).all()
+
+    station_list = list(np.ravel(results_stat))
+
     return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    station_query = session.query(measurement.station, func.count(measurement.station)).\
-                    group_by(measurement.station).\
-                    order_by(func.count(measurement.station).desc()).all()
+    active_station_list = session.query(measurement.station, func.count(measurement.station)).\
+                         group_by(measurement.station).\
+                         order_by(func.count(measurement.station).desc()).all()
+    
+    active_station = active_station_list[0][0]
 
-    active_station = station_query[0][0]
-    active_station_name = session.query(station.name).filter(station.station == active_station)
-    active_station_name = active_station_name[0][0]
-
-    last_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
-    last_date = last_date[0]
-
-    first_date = dt.date(2017, 8, 23) - dt.timedelta(days = 365)
-
-    temp_data = session.query(measurement.date, measurement.tobs).\
-                filter(measurement.date >= first_date).\
+    results_temp = session.query(measurement.date, measurement.tobs).\
+                filter(measurement.date >= "2016-08-23").\
                 filter(measurement.station == active_station).\
                 order_by(measurement.date).all()
-    
-    temp_list = []
-    for t in temp_data:
-        temp_dict = {temp_data.date: temp_data.tobs, "Station": temp_data.station}
-        temp_list.append(temp_dict)
-    
-    return print(f"The most active station is {active_station}, {active_station_name}")
+
+    temp_list = list(np.ravel(results_temp))
+
     return jsonify(temp_list)
 
-@app.route("/api/v1.0/<start>")
-def start(start):
+@app.route("/api/v1.0/<start_date>")
+def start(start_date):
+    results_date = session.query(measurement.date, func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+                       filter(measurement.date >= start_date).all()
     
+    date_dict = {}
+    
+    for dt in results_date:
+        diction = {}
+        diction["Date"] = dt.date
+        diction["Min"] = dt.func.min(measurement.tobs)
+        diction["Max"] = dt.func.min(measurement.tobs)
+        diction["Average"] = dt.func.avg(measurement.tobs)
+        date_dict.append(diction)
+    
+    return jsonify(date_dict)
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def startend(start_date, end_date):
+    results_date_2 = session.query(measurement.date, func.min(measurement.tobs, func.max(measurement.tobs), func.avg(measurement.tobs)).\
+                        filter(measurement.date >= start_date).filter(measurement.date <= end_date).order_by(measurement.date).all()
+    
+    dates_dict = {}
+
+    for x in results_date_2:
+        dictionn = {}
+        dictionn["Date"] = x.results_date_2[0]
+        dictionn["Min"] = x.results_date_2[1]
+        dictionn["Max"] = x.results_date_2[2]
+        dictionn["Average"] = x.results_date_2[3]
+        dates_dict.append(dictionn)
+    
+    return jsonify(dates_dict)
+
+if __name__ == "__main__":
+    app.run(debug=True)
